93798eb72889cc66b4bae727f58d8499b84d435e
Akbarali <nematjonovakbarali19@gmail.com>
2023-12-18 06:13:55 -0800

    linked with html

--- index.js
+++ index.js
@@ -1,14 +1,39 @@
-const http = require('http')
+const http = require('http');
+const fs = require('fs')
+const path = require('path')
 
-const server = http.createServer((req, res)=>{
-    console.log(req.url);
+const server = http.createServer((req, res) => {
+    if (req.method === 'GET') {
+        res.writeHead(200, { 'Content-Type': 'text/html' })
 
+        if(req.url === '/'){
+            fs.readFile(path.join(__dirname, "templates", "index.html"), 'utf-8', (err, conten)=>{
+                if (err) throw err
+                res.end(conten)
+            })
+        }
+    }else if(req.method === 'POST'){
+        
+        const Body = []
 
-    res.write('<h1>HEllo</h1>')
-    res.end()
+        req.on('data', data =>{
+            Body.push(Buffer.from(data))
+        })
+
+        req.on('end', ()=>{
+            fs.readFile(path.join(__dirname, "templates", "succes.html"), "utf-8", (err, conten)=>{
+                if (err) {
+                    throw err
+                }
+
+                res.end(conten)
+            })
+        })
+
+    }
 })
 
 let port = 5550
-server.listen(port, ()=>{
+server.listen(port, () => {
     console.log(`Server Started on Port: ${port}`);
 })
