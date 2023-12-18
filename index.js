41a87168322c000d3e05b4b10aca05eaf0ddcdef
Akbarali <nematjonovakbarali19@gmail.com>
2023-12-18 07:27:36 -0800

    New

--- index.js
+++ index.js
@@ -4,24 +4,38 @@ const path = require('path')
 
 const server = http.createServer((req, res) => {
     if (req.method === 'GET') {
-        res.writeHead(200, { 'Content-Type': 'text/html' })
 
-        if(req.url === '/'){
-            fs.readFile(path.join(__dirname, "templates", "index.html"), 'utf-8', (err, conten)=>{
+        if (req.url === '/') {
+            fs.readFile(path.join(__dirname, "templates", "index.html"), 'utf-8', (err, conten) => {
                 if (err) throw err
                 res.end(conten)
             })
+        } else if (req.url === '/about') {
+            fs.readFile(path.join(__dirname, "templates", "about.html"), 'utf-8', (err, conten) => {
+                if (err) throw err
+                res.end(conten)
+            })
+        } else if (req.url === '/contact') {
+            fs.readFile(path.join(__dirname, "templates", "contact.html"), 'utf-8', (err, conten) => {
+                if (err) throw err
+                res.end(conten)
+            })
+        } else if (req.url === '/api/admin') {
+            res.writeHead(200, { 'Content-Type': 'text/json' })
+            const admin = {name: 'akbarali', surname: 'nematjonov', age: 14}
+
+            res.end(JSON.stringify(admin))
         }
-    }else if(req.method === 'POST'){
-        
+    } else if (req.method === 'POST') {
+
         const Body = []
 
-        req.on('data', data =>{
+        req.on('data', data => {
             Body.push(Buffer.from(data))
         })
 
-        req.on('end', ()=>{
-            fs.readFile(path.join(__dirname, "templates", "succes.html"), "utf-8", (err, conten)=>{
+        req.on('end', () => {
+            fs.readFile(path.join(__dirname, "templates", "succes.html"), "utf-8", (err, conten) => {
                 if (err) {
                     throw err
                 }
@@ -33,7 +47,7 @@ const server = http.createServer((req, res) => {
     }
 })
 
-let port = 5550
+let port = 3000
 server.listen(port, () => {
     console.log(`Server Started on Port: ${port}`);
 })
